c427bc335fe19908e0a4b194567ac8c7bfeef1bc
Adrian - Paul Bolte <Bolte.Il.Adrian@student.utcluj.ro>
2021-10-15 16:44:47 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        //sarpele o sa fie o lista de cercuri
+        private List<Circle> Snake = new List<Circle>();
+        //se creeaza un cerc pt food
+        private Circle food = new Circle();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Set settings to default
+            new Settings();
+
+            //Seteaza viteza si timer-ul jocului
+            gameTimer.Interval = 1000 / Settings.Speed;
+            gameTimer.Tick += UpdateScreen;
+            gameTimer.Start();
+
+            //incepe un nou joc
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            lblGameOver.Visible = false;
+
+            //Set settings to default
+            new Settings();
+
+            //Create new player object
+            
+            Snake.Clear();//stergem caracterul anterior
+            Circle head = new Circle {X = 10, Y = 5};//se creeaza noul caracter
+            Snake.Add(head);//aceste creste la fiecare mancare mancata
+
+
+            lblScore.Text = Settings.Score.ToString();//se afiseaza noul scor
+            GenerateFood();//genereaza o noua mancare
+
+            label3.Text = Settings.Speed.ToString();//se afiseaza viteza jocului
+
+        }
+
+        //plaseaza mancarea in zone random ale ecranului de joc
+        private void GenerateFood()
+        {
+            int maxXPos = pbCanvas.Size.Width / Settings.Width;
+            int maxYPos = pbCanvas.Size.Height / Settings.Height;
+
+            Random random = new Random();
+            food = new Circle {X = random.Next(0, maxXPos), Y = random.Next(0, maxYPos)};
+        }
+
+
+        
+        private void UpdateScreen(object sender, EventArgs e)
+        {
+           //verifica daca jocul s-a incheiat
+            if (Settings.GameOver)
+            {
+                //Check if Enter is pressed
+                if (Input.KeyPressed(Keys.Enter))
+                {
+                    StartGame();
+                }
+            }
+            else
+            {
+                //directiile caracterului
+                if (Input.KeyPressed(Keys.Right) && Settings.direction != Direction.Left)
+                    Settings.direction = Direction.Right;
+                else if (Input.KeyPressed(Keys.Left) && Settings.direction != Direction.Right)
+                    Settings.direction = Direction.Left;
+                else if (Input.KeyPressed(Keys.Up) && Settings.direction != Direction.Down)
+                    Settings.direction = Direction.Up;
+                else if (Input.KeyPressed(Keys.Down) && Settings.direction != Direction.Up)
+                    Settings.direction = Direction.Down;
+
+                MovePlayer();
+            }
+
+            pbCanvas.Invalidate();//se asigura ca toate datele vor fi sterse
+
+        }
+
+        private void pbCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics canvas = e.Graphics;
+
+            if (!Settings.GameOver)
+            {
+                //seteaza culoarea sarpelui
+
+                int aux = 0;
+
+                for (int i = 0; i < Snake.Count; i++)
+                {
+                    Brush snakeColour;
+                    if (i == 0)
+                    {
+                        snakeColour = Brushes.Black;     //deseneaza capul
+                    }
+                    else
+                    {
+                        aux++;
+                        if(aux%2==0)
+                        snakeColour = Brushes.CornflowerBlue;    //deseneaza restul corpului daca este nr par albastru
+                        else
+                            snakeColour = Brushes.Chocolate;    //deseneaza restul corpului daca este un cerc cu nr impar e maro
+                    }
+                    //deseneaza sarpele
+                    canvas.FillEllipse(snakeColour,
+                        new Rectangle(Snake[i].X * Settings.Width,
+                                      Snake[i].Y * Settings.Height,
+                                      Settings.Width, Settings.Height));
+
+
+                    //deseneaza mancarea
+                    canvas.FillEllipse(Brushes.Red,
+                        new Rectangle(food.X * Settings.Width,
+                             food.Y * Settings.Height, Settings.Width, Settings.Height));
+
+                }
+            }
+            else
+            {
+                string gameOver = "Game over \nScorul final este: " + Settings.Score + "\nApasati tasta 'ENTER' \npentru a incepe din nou.";
+                lblGameOver.Text = gameOver;
+                lblGameOver.Visible = true;
+            }
+        }
+
+
+        private void MovePlayer()
+        {
+            for (int i = Snake.Count - 1; i >= 0; i--)
+            {
+                //Move head
+                if (i == 0)
+                {
+                    switch (Settings.direction)
+                    {
+                        case Direction.Right:
+                            Snake[i].X++;
+                            break;
+                        case Direction.Left:
+                            Snake[i].X--;
+                            break;
+                        case Direction.Up:
+                            Snake[i].Y--;
+                            break;
+                        case Direction.Down:
+                            Snake[i].Y++;
+                            break;
+                    }
+
+
+                    //obtine maxim X si Y Pos
+                    int maxXPos = pbCanvas.Size.Width / Settings.Width;
+                    int maxYPos = pbCanvas.Size.Height / Settings.Height;
+
+                    //detecteaza daca sarpele s-a lovit de margine.
+                    if (Snake[i].X < 0 || Snake[i].Y < 0
+                        || Snake[i].X >= maxXPos || Snake[i].Y >= maxYPos)
+                    {
+                        Die();
+                    }
+
+
+                    //detecteaza daca sarpele s-a lovit de restul corpului.
+                    for (int j = 1; j < Snake.Count; j++)
+                    {
+                        if (Snake[i].X == Snake[j].X &&
+                           Snake[i].Y == Snake[j].Y)
+                        {
+                            Die();
+                        }
+                    }
+
+                    ////detecteaza daca sarpele a prins mancarea.
+                    if (Snake[0].X == food.X && Snake[0].Y == food.Y)
+                    {
+                        Eat();
+                    }
+
+                }
+                else
+                {
+                    //misca corpul
+                    Snake[i].X = Snake[i - 1].X;
+                    Snake[i].Y = Snake[i - 1].Y;
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Input.ChangeState(e.KeyCode, true);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            Input.ChangeState(e.KeyCode, false);
+        }
+
+        private void Eat()
+        {
+            //adauga inca un cerc la corp
+            Circle circle = new Circle
+            {
+                X = Snake[Snake.Count - 1].X,
+                Y = Snake[Snake.Count - 1].Y
+            };
+            Snake.Add(circle);
+
+            //Update la scor
+            Settings.Score += Settings.Points;
+            lblScore.Text = Settings.Score.ToString();
+
+            GenerateFood();
+        }
+
+        private void Die()
+        {
+            Settings.GameOver = true;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,39 @@
+﻿namespace Snake
+{
+    //tastele pentru miscare
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    };
+
+    public class Settings
+    {
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+        public static int Speed { get; set; }
+        public static int Score { get; set; }
+        public static int Points { get; set; }
+        public static bool GameOver { get; set; }
+        public static Direction direction { get; set; }
+
+        public Settings()
+        {//valorile de inceput
+            Width = 16;
+            Height = 16;
+
+            //setare viteza joc
+            Speed = 10;
+
+
+            Score = 0;
+            Points = 100;
+            GameOver = false;
+            direction = Direction.Down;
+        }
+    }
+
+
+}
